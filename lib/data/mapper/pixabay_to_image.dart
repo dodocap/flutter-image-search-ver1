e0a0a10e00f374ee85a10f006d623a178e13999b
dodocap <dodocap@naver.com>
2023-12-27 16:55:34 +0900

    feat: DTO to Model Mapper 생성

---
+++ lib/data/mapper/pixabay_to_image.dart
@@ -0,0 +1,12 @@
+import 'package:orm_image_search_ver1/data/dto/pixabay_dto.dart';
+import 'package:orm_image_search_ver1/data/model/image_item_model.dart';
+
+extension PixabayToImageMappger on Hits {
+  ImageItemModel mapper() {
+    return ImageItemModel(
+      imageUrl: previewURL ?? 'http://rooprekorea.co.kr/wp-content/uploads/2022/03/placeholder-4.png',
+      tag: tags ?? '',
+      likes: likes?.toInt() ?? 0,
+    );
+  }
+}
