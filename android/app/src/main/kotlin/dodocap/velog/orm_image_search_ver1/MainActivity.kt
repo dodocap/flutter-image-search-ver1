3f6c758e586a6e9996aca02f9430aeaa94c1ff91
dodocap <dodocap@naver.com>
2023-12-27 16:20:26 +0900

    initial commit

---
+++ android/app/src/main/kotlin/dodocap/velog/orm_image_search_ver1/MainActivity.kt
@@ -0,0 +1,6 @@
+package dodocap.velog.orm_image_search_ver1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '오름캠프 이미지 검색 Ver1',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
